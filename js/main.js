2c62e087c0fbe3118ea9e806fbc6c86f9fb80ab4
vanita22 <vanitayrafita2004@gmail.com>
2017-07-06 20:35:11 -0300

    cargando datos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-$('.carousel').carousel({
-  interval: 200;
-});
-
-$('#myCarousel').on('slide.bs.carousel', function () {
-  direction: left;
+$(document).ready(function(){
+  $('#carousel').carousel({
+  	interval: 1500, //sirve para regular la velocidad del slider.
+  	pause: false //indico que no hay pausa.
+});  
+  
 })
